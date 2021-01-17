4672d451e9cea72323420eca9e7a44973c3c8d47
Le Zhu <lz2093@nyu.edu>
2021-01-17 17:16:56 -0500

    + vol test

--- AssumptionTests.py
+++ AssumptionTests.py
@@ -4,14 +4,17 @@ import statistics
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 file_name = '500_FUTURES'
 #file_name = '300_FUTURES'
 #file_name = '50_FUTURES'
 
-raw_data = pd.read_excel('./data/'+file_name+'.xls')
+raw_data = pd.read_excel('./data/'+file_name+'.xls')#,parse_dates=True
 df = raw_data.drop_duplicates(subset=['TRADE_DT', 'UNDERLYING_CLOSE_PRICE'], keep='first').reset_index(drop=True)
 df['log_return'] = np.log(df.UNDERLYING_CLOSE_PRICE) - np.log(df.UNDERLYING_CLOSE_PRICE.shift(-1))
+df['TRADE_DT'] = df['TRADE_DT'].apply(str)
+df['TRADE_DT'] = pd.to_datetime(df['TRADE_DT'])#.dt.strftime('%Y-%m-%d')
 
 # vol for n future days
 def historical_val(series, window):
@@ -56,18 +59,50 @@ kurtosis = stats.kurtosis(df['log_return'].dropna())
 print('Kurtosis: ', kurtosis)
 
 # Stationary
+'''# GARCH
+plot_acf(df['log_return'].dropna() - df['log_return'].dropna().mean())
+#plot_pacf(df['log_return'].dropna() - df['log_return'].dropna().mean())
+plt.title("ACF"+file_name)
+plt.savefig("./results/ACF"+file_name+".png")
+plt.show()'''
+
+'''plt.plot(df['vol_21d'].dropna())
+plt.plot(np.ones(len(df['vol_21d'].dropna())) * df['vol_21d'].dropna().mean(), color='forestgreen', label='mean')
+#plt.plot(np.ones(len(df['vol_21d'].dropna())) * df['vol_21d'].dropna().mean() + df['vol_21d'].dropna().std()*df['vol_21d'].dropna().std()/2, color='lightcoral', label='+ 0.5 sigma^2')
+#plt.plot(np.ones(len(df['vol_21d'].dropna())) * df['vol_21d'].dropna().mean() - df['vol_21d'].dropna().std()*df['vol_21d'].dropna().std()/2, color='lightcoral', label='- 0.5 sigma^2')
+#plt.title("2 sigma boundary of vol_21d for " + file_name)
+plt.title("rolling vol_21d for " + file_name)
+plt.legend()
+plt.savefig("./results/2 sigma boundary of vol_21d for " + file_name + ".png")
+plt.show()'''
+
+'''df_vol = df[['TRADE_DT','vol_21d']].dropna()
+df[['TRADE_DT','vol_21d']].dropna().plot()
+plt.plot(df_vol['TRADE_DT'],df_vol['vol_21d'])'''
+plt.plot(df['vol_21d'])
+plt.plot(np.ones(len(df['vol_21d'])) * df['vol_21d'].mean(), color='forestgreen', label='mean')
+#plt.plot(np.ones(len(df['vol_21d'].dropna())) * df['vol_21d'].dropna().mean() + df['vol_21d'].dropna().std()*df['vol_21d'].dropna().std()/2, color='lightcoral', label='+ 0.5 sigma^2')
+#plt.plot(np.ones(len(df['vol_21d'].dropna())) * df['vol_21d'].dropna().mean() - df['vol_21d'].dropna().std()*df['vol_21d'].dropna().std()/2, color='lightcoral', label='- 0.5 sigma^2')
+#plt.title("2 sigma boundary of vol_21d for " + file_name)
+plt.title("rolling vol_21d for " + file_name)
+plt.legend()
+plt.savefig("./results/2 sigma boundary of vol_21d for " + file_name + ".png")
+plt.show()
+
+# print(df['vol_21d'])
+# print('-----------------------')
+# print(df['log_return'])
+
+
 # unit root
 # DF test
 # demean time series is white noise
 # Box-Ljung test
 # ACF
 
-#pd.set_option('display.max_columns', None)
-#print(df)
-#print(df.info())
-
-
 '''
+CSI500:
+
            vol_21d   vol_63d  vol_126d  vol_189d
 vol_21d   1.000000  0.507497  0.220444 -0.019787
 vol_63d   0.507497  1.000000  0.541787  0.314606
